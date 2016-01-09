8bb412c366ade5c3cde27581aff3c15067c674d6
John D <deblanc.john@gmail.com>
2016-01-09 09:06:32 -0600

    Adding more http request metrics to track

--- client/js/app.js
+++ client/js/app.js
@@ -20,12 +20,22 @@ $(document).ready(function(){
     $('#stats').html("Buffer level " + bufferLevel.toPrecision(3) + 's');
     var currentUrl = "empty";
     var bytes = 0;
+    var mediaDuration = 0;
+    var latency = null;
+    var interval = null;
     if(currentHttpRequest){
       currentUrl = currentHttpRequest.actualurl;
       bytes = currentHttpRequest._bytes;
+      mediaDuration = currentHttpRequest._mediaduration;
+      latency = currentHttpRequest._latency;
+      interval = currentHttpRequest.interval;
     }
     $('#currentUrl').html("Current Http Request URL: " + currentUrl);
     $('#currentBytes').html("Current Http Request Bytes: " + bytes);
+    $('#currentMediaDuration').html("Current Http Request Media Duration: " + mediaDuration);
+    $('#currentLatency').html("Current Http Request Latency: " + latency);
+    $('#currentInterval').html("Current Http Request Interval: " + interval);
+
   }
 
   init();
