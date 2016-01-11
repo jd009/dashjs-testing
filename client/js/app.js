b9ef0547525d35aa2c7ae1eaae250ea205097804
John D <deblanc.john@gmail.com>
2016-01-11 12:33:26 -0600

    Adding video bitrate and audio bitrate display.

--- client/js/app.js
+++ client/js/app.js
@@ -17,7 +17,24 @@ $(document).ready(function(){
     var bufferLevel = metricsExt.getCurrentBufferLevel(metrics);
     var currentHttpRequest = metricsExt.getCurrentHttpRequest(metrics);
 
-    $('#stats').html("Buffer level " + bufferLevel.toPrecision(3) + 's');
+    var currentDVRInfo = metricsExt.getCurrentDVRInfo(metrics);
+    var videoBitrateInfoList = player.getBitrateInfoListFor('video');
+    if(videoBitrateInfoList && videoBitrateInfoList.length){
+      var bitrateInfo = videoBitrateInfoList[0];
+      var bitrate = bitrateInfo.bitrate;
+      $('#currentVideoBitrate').html("Video bitrate: " + bitrate);
+    }
+
+    var bitrateInfoList = player.getBitrateInfoListFor('audio');
+    if(bitrateInfoList && bitrateInfoList.length){
+      var bitrateInfo = bitrateInfoList[0];
+      var bitrate = bitrateInfo.bitrate;
+      $('#currentAudioBitrate').html("Audio bitrate: " + bitrate);
+    }
+
+    if(bufferLevel){
+      $('#stats').html("Buffer level " + bufferLevel.level + 's');
+    }
     var currentUrl = "empty";
     var bytes = 0;
     var mediaDuration = 0;
@@ -25,23 +42,23 @@ $(document).ready(function(){
     var interval = null;
     if(currentHttpRequest){
       currentUrl = currentHttpRequest.actualurl;
-      bytes = currentHttpRequest._bytes;
-      mediaDuration = currentHttpRequest._mediaduration;
-      latency = currentHttpRequest._latency;
-      interval = currentHttpRequest.interval;
+      // bytes = currentHttpRequest._bytes;
+      // mediaDuration = currentHttpRequest._mediaduration;
+      // latency = currentHttpRequest._latency;
+      // interval = currentHttpRequest.interval;
     }
-    $('#currentUrl').html("Current Http Request URL: " + currentUrl);
-    $('#currentBytes').html("Current Http Request Bytes: " + bytes + " Kilobytes: " + (bytes / 1000).toFixed(1));
-    $('#currentMediaDuration').html("Current Http Request Media Duration: " + mediaDuration);
-    $('#currentLatency').html("Current Http Request Latency: " + latency);
-    $('#currentInterval').html("Current Http Request Interval: " + interval);
+    // $('#currentUrl').html("Current Http Request URL: " + currentUrl);
+    // $('#currentBytes').html("Current Http Request Bytes: " + bytes + " Kilobytes: " + (bytes / 1000).toFixed(1));
+    // $('#currentMediaDuration').html("Current Http Request Media Duration: " + mediaDuration);
+    // $('#currentLatency').html("Current Http Request Latency: " + latency);
+    // $('#currentInterval').html("Current Http Request Interval: " + interval);
 
     //https://github.com/Dash-Industry-Forum/dash.js/blob/development/samples/chromecast/receiver/js/main.js
-    // var repSwitch = metricsExt.getCurrentRepresentationSwitch(metrics);
-    // if (repSwitch !== null) {
-    //   var bandwidthValue = metricsExt.getBandwidthForRepresentation(repSwitch.to);
-    //   $('#currentVideoBitrate').html("Current Video Bitrate: " + bandwidthValue);
-    // }
+    var repSwitch = metricsExt.getCurrentRepresentationSwitch(metrics);
+    if (repSwitch !== null) {
+      //var bandwidthValue = metricsExt.getBandwidthForRepresentation(repSwitch.to);
+      //$('#currentVideoBitrate').html("Current Video Bitrate: " + bandwidthValue);
+    }
 
   }
 
